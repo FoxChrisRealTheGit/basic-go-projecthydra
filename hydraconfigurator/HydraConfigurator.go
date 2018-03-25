3f6c78548ef06f3ff510646d553640dd5cb8b59b
Christopher Fox <cfox1290@gmail.com>
2018-03-25 15:23:33 -0600

    added special config file

---
+++ hydraconfigurator/HydraConfigurator.go
@@ -0,0 +1,33 @@
+package HYDRACONFIGURATOR
+//entry point for package
+import (
+	"errors"
+	"reflect"
+)
+
+//will allow for other file formats
+const (
+	CUSTOM uint8 = iota
+)
+
+// if type is nto a struct then reflect cannot be set
+var wrongTypeError error = errors.New("Type must be a pointer to a struct")
+
+func GetConfiguration(confType uint8, obj interface{}, filename string) (err error){
+	//check if this is a type pointer
+	mysRValue := reflect.ValueOf(obj)
+	if mysRValue.Kind() != reflect.Ptr || mysRValue.IsNil(){
+		return wrongTypeError
+	}
+	//get an dconfirm the struct value
+	mysRValue = mysRValue.Elem()
+	if mysRValue.Kind() != reflect.Struct{
+		return wrongTypeError
+	}
+
+	switch confType{
+	case CUSTOM:
+		err = MarshalCustomConfig(mysRValue, filename)
+	}
+	return err
+}
